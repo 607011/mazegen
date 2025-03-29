eded70b2806d793b7d1d7e92ff51343a77694c95
Oliver Lau <oliver@ersatzworld.net>
2025-03-29 16:53:10 +0100

    Improved command-line arguments.

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,25 @@ mod maze;
 use maze::{Exit, Maze};
 
 #[derive(clap::Parser, Debug)]
-#[command(name = "maze", version = "0.1.0", about = "Generates and solves mazes")]
+#[command(name = "maze", version = "0.1.0", about = "Generate and solve mazes")]
 struct Cli {
-    #[arg(short, long, default_value_t = 40)]
+    #[arg(short, long, default_value_t = 60, help = "Width of the maze")]
     width: usize,
-    #[arg(short, long, default_value_t = 30)]
+    #[arg(short, long, default_value_t = 30, help = "Height of the maze")]
     height: usize,
-    #[arg(short, long, default_value_t = 3)]
+    #[arg(short, long, default_value_t = 3, help = "Size if the central room")]
     room_size: usize,
-    #[arg(short, long, default_value_t = 0.07)]
+    #[arg(short, long, default_value_t = 0.07, help = "Percentage of the maze to fill with artifacts")]
     fill_percentage: f32,
+    #[arg(short, long, help = "Output maze to DOT file for GraphViz")]
+    dot_file: Option<String>,
+    #[arg(short, long, help = "Output maze to SVG file")]
+    svg_file: Option<String>,
     #[arg(long, default_value_t = 10.0)]
     scale: f32,
-    #[arg(long)]
-    solution: bool,
-    #[arg(short, long)]
+    #[arg(long, help = "Show solution path in SVG output")]
+    with_solution: bool,
+    #[arg(short, long, default_value_t = false, help = "Enable verbose output")]
     verbose: bool,
 }
 
@@ -26,8 +30,12 @@ fn main() {
     let cli = Cli::parse();
     let mut maze = Maze::new(cli.width, cli.height, cli.room_size, Some(Exit::Right));
     maze.place_artifacts(cli.fill_percentage);
-    maze.export_to_dot("maze.dot")
-        .expect("Failed to export maze to DOT file");
-    maze.export_to_svg("maze.svg", cli.scale, cli.solution)
-        .expect("Failed to export maze to SVG file");
+    if let Some(dot_file) = cli.dot_file {
+        maze.export_to_dot(&dot_file)
+            .expect("Failed to export maze to DOT file");
+    }
+    if let Some(svg_file) = cli.svg_file {
+        maze.export_to_svg(&svg_file, cli.scale, cli.with_solution)
+            .expect("Failed to export maze to SVG file");
+    }
 }
