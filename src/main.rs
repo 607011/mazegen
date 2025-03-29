67d6610cf56c9e8117ac6192faa4f7b29aea28d5
Oliver Lau <oliver@ersatzworld.net>
2025-03-29 17:12:30 +0100

    Further improved command-line args.

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,19 @@ struct Cli {
     height: usize,
     #[arg(short, long, default_value_t = 3, help = "Size if the central room")]
     room_size: usize,
-    #[arg(
-        short,
-        long,
-        default_value_t = 0.07,
-        help = "Percentage of the maze to fill with artifacts"
-    )]
-    fill_percentage: f32,
+    #[arg(short, long, help = "Percentage of the maze to fill with artifacts")]
+    artifacts_percentage: Option<f32>,
     #[arg(short, long, help = "Output maze to DOT file for GraphViz")]
     dot_file: Option<String>,
     #[arg(short, long, help = "Output maze to SVG file")]
     svg_file: Option<String>,
     #[arg(long, default_value_t = 10.0)]
     scale: f32,
-    #[arg(long, help = "Show solution path in SVG output")]
+    #[arg(
+        long,
+        default_value_t = true,
+        help = "Show solution path in SVG output"
+    )]
     with_solution: bool,
     #[arg(short, long, default_value_t = false, help = "Enable verbose output")]
     verbose: bool,
@@ -34,7 +33,9 @@ struct Cli {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
     let mut maze = Maze::new(cli.width, cli.height, cli.room_size, Some(Exit::Right));
-    maze.place_artifacts(cli.fill_percentage);
+    if let Some(artifacts_percentage) = cli.artifacts_percentage {
+        maze.place_artifacts(artifacts_percentage);
+    }
     if let Some(dot_file) = cli.dot_file {
         maze.export_to_dot(&dot_file)?;
     }
